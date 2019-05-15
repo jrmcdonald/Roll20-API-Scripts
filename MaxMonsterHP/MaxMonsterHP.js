65234e625a405163f54a15717650cdc6b974d8ca
Jamie McDonald <jamie@qwyck.net>
2019-05-15 23:31:59 +0100

    Initial version complete.

--- MaxMonsterHP/MaxMonsterHP.js
+++ MaxMonsterHP/MaxMonsterHP.js
@@ -10,109 +10,200 @@
  * @license MIT
  */
 
-var MaxMonsterHP = MaxMonsterHP || (function() {
+var MaxMonsterHP = MaxMonsterHP || (() => {
     'use strict';
     
     const VERSION = '0.0.1';
     
     const CHAT_NAME = 'MaxMonsterHP';
     const CHAT_COMMAND = '!mmhp';
-
     const CHAT_OPT_SEL = '--fix-selected';
     const CHAT_OPT_ALL = '--fix-all';
+
+    const BAR = "bar1";
     
     let tokenIds = [];
 
-    const checkInstall = function() {
+    /**
+     * Log script info to the console.
+     */
+    const checkInstall = () => {
         log('-=> MaxMonsterHP v'+VERSION+' <=-');
     };
 
-    const handleInput = function(msg) {
-        var args;
-
-        if (msg.type !== "api") {
-            return;
-        }
-
-        args = msg.content.split(/\s+/);
-        switch(args[0]) {
-            case '!mmh':
-                break;
-        }
+    /**
+     * Display in-game help to the chat window.
+     * 
+     * @returns A formatted chat template containing help information.
+     */
+    const displayHelp = () => {
+        return '&{template:default} {{name=Max Monster HP}} {{Usage=!mmhp [options]}} {{Options=--fix-selected\n--fix-all}}';
     };
 
-    const parseValue = function(val) {
-        return parseInt(val || "1");
-    };
+    /**
+     * Parses a value into an int.
+     *  
+     * @param {any} val the value to parse
+     * @returns The int value.
+     */
+    const parseValue = val => parseInt(val || "1");
     
-    const parseExpression = function(expression) {
-        log("Parsing " + expression);
-      
-        var parts = expression.split(/d/);
-        var sum = 0;
-        var limit = parseValue(parts[1]);
-        
-        for (var i = parseValue(parts[0]) - 1; i >= 0; i--) {
-            sum += limit;      
+    /**
+     * Calculate the max value of a hit dice expression.
+     * 
+     * For example 2d6 = 12.
+     *  
+     * @param {any} expression the hit dice expression to calculate the maximum of
+     * @returns the calculated value
+     */
+    const parseExpression = expression => expression.split(/d/).reduce((a,b) => parseValue(a) * parseValue(b));
+
+    /**
+     * Check if the supplied character id is an NPC or not.
+     * 
+     * @param {any} id the character id
+     * @returns true or false
+     */
+    const isNpc = id => {
+        let isNpc = false;
+        const checkNpc = findObjs({ _type: 'attribute', _characterid: id, name: 'npc' });
+
+        if (!_.isUndefined(checkNpc[0])) {
+            if (checkNpc[0].get('current') === '1') {
+                isNpc = true;
+            }
         }
-        
-        return sum;
+
+        return isNpc;
     };
 
-    const setMaxHP = function(obj) {
-        var sets = {},
-            bar = 'bar1',
-            hdAttrib,
-            hdExpression = 0,
-            hp = 0
-            ;
+    /**
+     * Fetch the hit dice expression.
+     *  
+     * @param {any} obj the object to get the hit dice expression from
+     * @returns the hit dice expression
+     */
+    const getExpression = obj => {
+        let hdExpression = 0;
 
-        if(_.contains(tokenIds, obj.id)) {
-            tokenIds=_.without(tokenIds,obj.id);
+        const hdAttrib = findObjs({
+            type: 'attribute', 
+            characterid: obj.get('represents'),
+            name: 'npc_hpformula'
+        })[0];
+        
+        if (hdAttrib) {
+            hdExpression = hdAttrib.get('current');
+        }
 
-            if('graphic' === obj.get('type') && 'token' === obj.get('subtype') && '' !== obj.get('represents')) {
+        return hdExpression;
+    };
+
+    /**
+     * Set the maximum hp value on a token. 
+     * 
+     * @param {any} obj the object to update
+     */
+    const setMaxHp = obj => {
+        if(obj && 'graphic' === obj.get('type') 
+            && 'token' === obj.get('subtype') 
+            && '' !== obj.get('represents')
+            && '' === obj.get('bar1_link')
+            && isNpc(obj.get('represents'))) {
+
+                let expression = getExpression(obj); 
+                let hp = expression.replace(/[^+0-9d]+/g, "")
+                            .split(/\+/)
+                            .map(parseExpression)
+                            .reduce((a, b) => parseValue(a) + parseValue(b));
                 
-                if(obj && '' === obj.get(bar+'_link')) {
-                    hdAttrib = findObjs({
-                        type: 'attribute', 
-                        characterid: obj.get('represents'),
-                        name: npc_hpformula
-                    })[0];
-                    
-                    if (hdAttrib) {
-                        hdExpression = hdAttrib.get('current');
-                        
-                        hp = hdExpression.replace(/[^+0-9d]+/g, "")
-                                         .split(/\+/)
-                                         .map(parseExpression)
-                                         .reduce(function(a,b){return a + b;});
-                        
-                        sets[bar+"_value"] = hp||1;
-                        sets[bar+"_max"] = hp||1;
-                        obj.set(sets);
-                    }
+                log(`Setting HP for ${obj.get('name')} to ${hp} (${expression}).`);                
+                obj.set({bar1_value: hp || 1, bar1_max: hp || 1});
+        }
+    }
+
+    /**
+     * Handle the change:graphic event.
+     *  
+     * @param {*} obj the object being changed
+     */
+    const handleChangeGraphic = obj => {
+        if(_.contains(tokenIds, obj.id)) {
+            tokenIds = _.without(tokenIds, obj.id);
+            setMaxHp(obj);
+        }
+    };
+
+    /**
+     * Handle the add:graphic event.
+     *  
+     * @param {*} obj the object being added
+     */
+    const handleAddGraphic = obj => {
+        tokenIds.push(obj.id);
+
+        setTimeout((function(id) {
+            return function() {
+                let token = getObj('graphic', id);
+                if (token) {
+                    handleChangeGraphic(token);
                 }
+            };
+        }(obj.id)), 100);
+    };
+
+    /**
+     * Wrapper for whispering a response to the player.
+     * 
+     * @param {any} msg the original chat message
+     * @param {any} response the response to send
+     */
+    const respond = (msg, response) => {
+        sendChat(CHAT_NAME, "/w " + msg.who + " " + response);
+    };
+
+    /**
+     * Handler for chat input. Detects script triggers and checks for optional 
+     * arguments.
+     * 
+     * @param {any} msg the chat message
+     * @returns
+     */    
+    const handleChatMessage = msg => {
+        if (msg.type === 'api' && playerIsGM(msg.playerid) && msg.content.startsWith(CHAT_COMMAND)) {
+            const args = msg.content.split(' ');
+
+            if (args.length != 2) {
+                respond(msg, displayHelp());
+                respond(msg, "**Please specify 1 option.");
+                return;
+            }
+            
+            if (args.includes(CHAT_OPT_SEL) && msg.selected) {
+                _.each(msg.selected, selected => {
+                    setMaxHp(getObj(selected._type, selected._id));
+                });
+                return;
+            }
+            
+            if (args.includes(CHAT_OPT_ALL)) {
+                filterObjs((o) => {
+                    if ('graphic' === o.get('type') && 'token' === o.get('subtype') && '' !== o.get('represents')) {
+                        setMaxHp(o);
+                    }
+                });
+                return;
             }
         }
     };
-
-    const saveTokenId = function(obj){
-        tokenIds.push(obj.id);
-
-        setTimeout((function(id){
-            return function(){
-                var token=getObj('graphic',id);
-                if(token){
-                    setMaxHP(token);
-                }
-            };
-        }(obj.id)),100);
-    };
-
-    const registerEventHandlers = function() {
-        on('chat:message', handleInput);
-        on('add:graphic', saveTokenId);
-        on('change:graphic', setMaxHP);
+    
+    /**
+     * Register event handlers
+     */
+    const registerEventHandlers = () => {
+        on('chat:message', handleChatMessage);
+        on('add:graphic', handleAddGraphic);
+        on('change:graphic', handleChangeGraphic);
     };
 
     return {
@@ -120,7 +211,7 @@ var MaxMonsterHP = MaxMonsterHP || (function() {
         RegisterEventHandlers: registerEventHandlers
     };
     
-}());
+})();
 
 on('ready',function() {
     'use strict';
